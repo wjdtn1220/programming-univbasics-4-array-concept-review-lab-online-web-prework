6fb852c47fd3bcee2f2efb1c3ffbb1551b3acfcc
Jeong Park <wjdtn1220@gmail.com>
2019-07-29 23:10:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,14 +19,14 @@ end
 
 def find_min_value(array)
   min = 0
-    array.length.times do |count|
-      if count == 0
-       min = array[count]
-      else
-       if array[count < min
-         min = array[count]
-       end
-     end
+  array.length.times do |count|
+    if count == 0
+     min = array[count]
+    else
+      if array[count] < min
+        min = array[count]
+      end
+    end
   end
   return min
 end
