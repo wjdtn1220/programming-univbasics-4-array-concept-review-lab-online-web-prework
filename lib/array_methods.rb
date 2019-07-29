55af537164887ef1519efbb023bc9dd429e2c1ec
Jeong Park <wjdtn1220@gmail.com>
2019-07-29 23:00:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,32 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |count|
-    if array[count] == value_to_find
-      return count
-    end
-  end
-  return nil
-end
+  array.length times
 
 def find_max_value(array)
-  max = 0
-    array.length.times do |count|
-      if array[count] > max
-        max = array[count]
-      end
-    end
-  return max
-end
+
 
 def find_min_value(array)
-  min = 0
-    array.length.times do |count|
-      if count == 0
-        min = array[count]
-      else
-        if array[count] < min
-          min = array[count]
-        end
-      end
-    end
-  return min
-end
+
