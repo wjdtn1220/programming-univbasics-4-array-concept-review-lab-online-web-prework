c55c97f264f4e60bc283bb577fe4204780699986
Jeong Park <wjdtn1220@gmail.com>
2019-07-29 23:00:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array.length.times do |count|
-    if array.length == value_to_find
+    if array == value_to_find
       return 
 
 def find_max_value(array)
