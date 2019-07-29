40ef099ce6ebfc3f83293c2430c0150f8a94643f
Jeong Park <wjdtn1220@gmail.com>
2019-07-29 23:04:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,25 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
+  max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  return max
+end
 
 def find_min_value(array)
-
+  min = 0
+    array.length.times do |count|
+      if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  return min
+end
